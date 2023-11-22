6f55d179245e85fe64c97905db53298951c1d610
Tetiana Dub <112940506+Tasha852209@users.noreply.github.com>
2023-11-22 18:47:23 +0200

    all

--- src/4.ts
+++ src/4.ts
@@ -1,4 +1,41 @@
-const key = new Key();
+class Key {
+  constructor(private signature: number) {}
+  getSignature(): number {
+    return this.signature;
+  }
+}
+class Person {
+  constructor(private key: Key) {}
+  getKey(): Key {
+    return this.key;
+  }
+}
+abstract class House {
+  door: boolean;
+  tenants: Person[] = [];
+
+  constructor(public key: Key) {
+    this.door = false;
+  }
+  comeIn(person: Person) {
+    if (this.door) {
+      this.tenants.push(person);
+    }
+    return;
+  }
+  abstract openDoor(key: Key): void;
+}
+
+class MyHouse extends House {
+  openDoor(key: Key): void {
+    if (key.getSignature() === this.key.getSignature()) {
+      this.door = true;
+    }
+    return;
+  }
+}
+
+const key = new Key(Math.floor(Math.random()));
 
 const house = new MyHouse(key);
 const person = new Person(key);
@@ -6,6 +43,5 @@ const person = new Person(key);
 house.openDoor(person.getKey());
 
 house.comeIn(person);
-
 
 export {};
